86cf65e713718a8c20a66b9da938603138adca20
ahmdpt101 <ahmdpt101@gmail.com>
2023-11-30 23:51:08 +0300

    Setup some routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,25 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 
+
+// Group for user-related routes with 'user' prefix
+Route::group(['prefix' => 'user'], function () {
+    // Route for user profile
+    Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
+
+    // Add more user-related routes here
+});
+
+// Group for blog-related routes with 'blog' prefix
+Route::group(['prefix' => 'blogs'], function () {
+    // Route for displaying all blogs
+    Route::get('/', [PageController::class, 'index'])->name('blog.index');
+
+    // Route for displaying a specific blog
+    Route::get('/{id}', [PageController::class, 'show'])->name('blog.show');
+
+    // Add more blog-related routes here
+});
 
 Auth::routes();
 
